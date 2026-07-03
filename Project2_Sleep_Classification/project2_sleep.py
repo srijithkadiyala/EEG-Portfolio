941582e93172bff96af28f5212d0a138f7496ded
srijithkadiyala <srijithkadiyala2@gmail.com>
2026-07-03 15:01:14 +0100

    light vs deep sleep comparision

--- Project2_Sleep_Classification/project2_sleep.py
+++ Project2_Sleep_Classification/project2_sleep.py
@@ -7,6 +7,40 @@ annotations = mne.read_annotations('SC4001EC-Hypnogram.edf')
 raw.set_annotations(annotations)
 print("Sleep stages loaded")
 print(annotations)
+for ann in annotations:
+    print(f"{ann['description']} startsat {ann['onset']:.0f} seconds")
+    
+fs_real= int (raw.info['sfreq'])
+    
+light_start = 30630 * fs_real
+light_end = light_start + (30 * fs_real)
+deep_start = 31350 * fs_real
+deep_end = deep_start + (30 * fs_real)
+    
+data_light = raw.get_data(picks='EEG Fpz-Cz')[0, light_start:light_end]
+data_deep = raw.get_data(picks='EEG Fpz-Cz')[0, deep_start:deep_end]
+    
+print(f"Light sleep chunk: {len(data_light)} samples")
+print(f"Deep sleep chunk: {len(data_deep)} samples")
+
+t_chunk = np.linspace(0, 30, 3000)
+fig, axes = plt.subplots(2,1, figsize=(2, 6))
+
+axes[0].plot(t_chunk, data_light * 1e6, color='blue')
+axes[0].set_title('Light Sleep - Stage 1 (30 seconds)')
+axes[0].set_xlabel('Time (seconds)')
+axes[0].set_ylabel('Amplitude (µV)')
+
+axes[1].plot(t_chunk, data_deep * 1e6, color='green')
+axes[1].set_title('Deep Sleep - Stage 4 (30 seconds)')
+axes[1].set_xlabel('Time (seconds)')
+axes[1].set_ylabel('Amplitude (µV)')
+
+plt.tight_layout()
+plt.savefig('light_vs_deep_sleep.png', dpi=150, bbox_inches='tight')
+plt.show()
+print("Comparision graph saved")
+
 eeg_channel= raw.pick_channels(['EEG Fpz-Cz'])
 data, time = raw[0, :3000]
 plt.figure(figsize=(2, 4))
